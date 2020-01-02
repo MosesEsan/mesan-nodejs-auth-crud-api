717e1747a1f4e3e4d970658b6d7a38ff9feca418
Moses Esan <mosesesan@hotmail.com>
2020-01-02 23:55:18 +0000

    Updated recover and resetPassword function to use sendEmail function declared in utils

--- src/controllers/password.js
+++ src/controllers/password.js
@@ -3,6 +3,8 @@ const User = require('../models/user');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+const {sendEmail} = require('../utils/index');
+
 // @route POST api/auth/recover
 // @desc Recover Password - Generates token and Sends password reset email
 // @access Public
@@ -18,26 +20,20 @@ exports.recover = async (req, res) => {
         user.generatePasswordReset();
 
         // Save the updated user object
-        user.save()
-            .then(user => {
-                // send email
-                let link = "http://" + req.headers.host + "/api/auth/reset/" + user.resetPasswordToken;
-                const mailOptions = {
-                    to: user.email,
-                    from: process.env.FROM_EMAIL,
-                    subject: "Password change request",
-                    text: `Hi ${user.username} \n 
-                    Please click on the following link ${link} to reset your password. \n\n 
-                    If you did not request this, please ignore this email and your password will remain unchanged.\n`,
-                };
+        await user.save();
 
-                sgMail.send(mailOptions, (error, result) => {
-                    if (error) return res.status(500).json({message: error.message});
+        // send email
+        let subject = "Password change request";
+        let to = user.email;
+        let from = process.env.FROM_EMAIL;
+        let link = "http://" + req.headers.host + "/api/auth/reset/" + user.resetPasswordToken;
+        let html = `<p>Hi ${user.username}</p>
+                    <p>Please click on the following <a href="${link}">link</a> to reset your password.</p> 
+                    <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>`;
 
-                    res.status(200).json({message: 'A reset email has been sent to ' + user.email + '.'});
-                });
-            })
-            .catch(err => res.status(500).json({message: err.message}));
+        await sendEmail({to, from, subject, html});
+
+        res.status(200).json({message: 'A reset email has been sent to ' + user.email + '.'});
     } catch (error) {
         res.status(500).json({message: error.message})
     }
@@ -61,39 +57,37 @@ exports.reset = async (req, res) => {
     }
 };
 
-
 // @route POST api/auth/reset
 // @desc Reset Password
 // @access Public
-exports.resetPassword = (req, res) => {
-    User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}})
-        .then((user) => {
-            if (!user) return res.status(401).json({message: 'Password reset token is invalid or has expired.'});
+exports.resetPassword = async (req, res) => {
+    try {
+        const { token } = req.params;
 
-            //Set the new password
-            user.password = req.body.password;
-            user.resetPasswordToken = undefined;
-            user.resetPasswordExpires = undefined;
-            user.isVerified = true;
+        const user = await User.findOne({resetPasswordToken: token, resetPasswordExpires: {$gt: Date.now()}});
 
-            // Save
-            user.save((err) => {
-                if (err) return res.status(500).json({message: err.message});
+        if (!user) return res.status(401).json({message: 'Password reset token is invalid or has expired.'});
 
-                // send email
-                const mailOptions = {
-                    to: user.email,
-                    from: process.env.FROM_EMAIL,
-                    subject: "Your password has been changed",
-                    text: `Hi ${user.username} \n 
-                    This is a confirmation that the password for your account ${user.email} has just been changed.\n`
-                };
+        //Set the new password
+        user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        user.isVerified = true;
 
-                sgMail.send(mailOptions, (error, result) => {
-                    if (error) return res.status(500).json({message: error.message});
+        // Save the updated user object
+        await user.save();
 
-                    res.status(200).json({message: 'Your password has been updated.'});
-                });
-            });
-        });
+        let subject = "Your password has been changed";
+        let to = user.email;
+        let from = process.env.FROM_EMAIL;
+        let html = `<p>Hi ${user.username}</p>
+                    <p>This is a confirmation that the password for your account ${user.email} has just been changed.</p>`
+
+        await sendEmail({to, from, subject, html});
+
+        res.status(200).json({message: 'Your password has been updated.'});
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 };
